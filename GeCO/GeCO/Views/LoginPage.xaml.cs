420ea6e935fba6bd81936e8431b2fc9a14c4cf6f
Dário Lopes <drafaelplopes@hotmail.com>
2018-09-03 15:47:26 +0100

    Login functionality added and PessoasListPage added (empty);

--- GeCO/GeCO/Views/LoginPage.xaml.cs
+++ GeCO/GeCO/Views/LoginPage.xaml.cs
@@ -23,10 +23,12 @@ namespace GeCO.Views{
 
         async void OnSignInClicked(object sender, System.EventArgs e){
             IsEnabled = false;
-            if (true){
+            if (entryUsername.Text == "admin" && entryPassword.Text == "teste123"){
                 await DisplayAlert("Login", "Login successful", "Ok");
                 var page = new MainMasterPage();
                 Application.Current.MainPage = page;        // MainPage inicial é LoginPage. Com um login bem sucedido basta trocar MainPage para a pagina desejada. 
+            } else {
+                await DisplayAlert("Login", "Login failed. Wrong credentials", "Try again.");
             }
             IsEnabled = true;
         }
